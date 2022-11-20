6a0da9c33f8c1f8211890ebf3261a4b4d3f4b1ce
Ariel Doyle <ariel.rose.garrett@gmail.com>
2022-11-19 19:24:26 -0800

    successfully link UI and Business logic to accept user input and display array with 1, 2, 3 replaced with prompt phrases.

--- js/scripts.js
+++ js/scripts.js
@@ -12,45 +12,49 @@ function countToValue(number) {
     return false;
   }
   return newArray;
-  console.log(newArray);
 }
 
 // This will need to use array.map!
 function robogerIt(number) {
   let numToString = "" + number;
   if (numToString.includes("3")) {
-    return "Won't you be my neighbor?";
+    return " Won't you be my neighbor?";
   } else if (numToString.includes("2")) {
-    return "Boop";
+    return " Boop!";
   } else if (numToString.includes("1")) {
-    return "Beep";
+    return " Beep!";
   } else {
-    return number;
+    return (" " + number);
   }
 }
 
-window.addEventListener("load", function() {
+function handleFormSubmission(e) {
+  e.preventDefault();
   const form = document.querySelector("form");
   const startBtn = document.getElementById("start-btn");
   const tryAgainBtn = document.getElementById("reset-btn");
   const inputDiv = document.getElementById("input-field")
   const formDiv = document.getElementById("form-div");
-  let input = document.getElementById("input-box").value;
-  const arrayFromInput = countToValue(input);
 
+  function addArrayToList() {
+
+  }
 
   document.body.addEventListener("click", function() {
     formDiv.removeAttribute("class");
     startBtn.classList.add("hidden");
   });
 
-  form.addEventListener("submit", function(e) {
-    e.preventDefault(); 
+  form.addEventListener("submit", function() {
+    let input = document.getElementById("input-box").value;
+    const arrayFromInput = countToValue(input);
+    const arrayWithWordsAdded = arrayFromInput.map(robogerIt);
     inputDiv.classList.add("hidden");
-    document.getElementById("output").innerText = arrayFromInput;
+    document.getElementById("output").innerText = arrayWithWordsAdded;
   });
+}
 
 
-
-
-})
+window.addEventListener("load", function() {
+  document.querySelector("form#form-div").addEventListener("submit", handleFormSubmission);
+});
