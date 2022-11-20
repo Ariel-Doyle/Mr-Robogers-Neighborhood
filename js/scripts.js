27311e0c1200d54e38189d9c9071c5f6298eb95f
Ariel Doyle <ariel.rose.garrett@gmail.com>
2022-11-19 20:02:34 -0800

    add a newline after each array object to divvy array into a list format.

--- js/scripts.js
+++ js/scripts.js
@@ -14,17 +14,16 @@ function countToValue(number) {
   return newArray;
 }
 
-// This will need to use array.map!
 function robogerIt(number) {
   let numToString = "" + number;
   if (numToString.includes("3")) {
-    return " Won't you be my neighbor?";
+    return "Won't you be my neighbor?\n";
   } else if (numToString.includes("2")) {
-    return " Boop!";
+    return "Boop!\n";
   } else if (numToString.includes("1")) {
-    return " Beep!";
+    return "Beep!\n";
   } else {
-    return (" " + number);
+    return (number + "\n");
   }
 }
 
@@ -36,15 +35,6 @@ function handleFormSubmission(e) {
   const inputDiv = document.getElementById("input-field")
   const formDiv = document.getElementById("form-div");
 
-  function addArrayToList(number) {
-    const p = document.getElementById("output");
-    const ul = document.createElement("ul");
-    const li = document.createElement("li");
-    let numToString = "" + number;
-    for (i=0; i < numToString.length; i+=1) {
-      
-  }
-
   document.body.addEventListener("click", function() {
     formDiv.removeAttribute("class");
     startBtn.classList.add("hidden");
@@ -55,7 +45,7 @@ function handleFormSubmission(e) {
     const arrayFromInput = countToValue(input);
     const arrayWithWordsAdded = arrayFromInput.map(robogerIt);
     inputDiv.classList.add("hidden");
-    document.getElementById("output").innerText = arrayWithWordsAdded;
+    document.getElementById("output").innerText = arrayWithWordsAdded.join(" ");
   });
 }
 
