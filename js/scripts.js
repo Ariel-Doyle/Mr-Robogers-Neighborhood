7216de96dc8b381ca31060fa9ec00887898340d1
Ariel Doyle <ariel.rose.garrett@gmail.com>
2022-11-27 18:23:51 -0800

    fix issue so that start button is only pressed once to access input field.

--- js/scripts.js
+++ js/scripts.js
@@ -27,27 +27,36 @@ function robogerIt(number) {
   }
 }
 
-function handleFormSubmission(e) {
-  e.preventDefault();
-  const form = document.querySelector("form");
+window.addEventListener("load", function() {
   const showNumbersBtn = document.getElementById("show-results-btn");
   const tryAgainBtn = document.getElementById("reset-btn");
   const inputDiv = document.getElementById("input-header");
   const resultDiv = document.getElementById("count-results");
-  const formDiv = document.getElementById("input-field");
+  const formFieldDiv = document.getElementById("input-field");
+  const form = document.querySelector("form");
+  const formDiv = document.getElementById("form-div");
+  const startBtn = document.getElementById("start-btn");
+
+  startBtn.addEventListener("click", function() {
+    formDiv.removeAttribute("class");
+    startBtn.classList.add("hidden");
+  });
 
   showNumbersBtn.addEventListener("click", function() {
     inputDiv.classList.add("hidden");
-    formDiv.classList.add("hidden");
+    formFieldDiv.classList.add("hidden");
     resultDiv.classList.remove("hidden");
   });
+
   tryAgainBtn.addEventListener("click", function() {
     inputDiv.classList.remove("hidden");
-    formDiv.classList.remove("hidden");
+    formFieldDiv.classList.remove("hidden");
     resultDiv.classList.add("hidden");
-  });  
+  });
 
-  form.addEventListener("submit", function() {
+
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
     let input = document.getElementById("input-box").value;
     const arrayFromInput = countToValue(input);
     const arrayWithWordsAdded = arrayFromInput.map(robogerIt);
@@ -55,18 +64,4 @@ function handleFormSubmission(e) {
     inputDiv.classList.add("hidden");
     document.getElementById("output").innerText = arrayWithWordsAdded.join(" ");
   });
-}
-
-window.addEventListener("load", function() {
-  const formDiv = document.getElementById("form-div");
-  const startBtn = document.getElementById("start-btn");
-
-  document.body.addEventListener("click", function() {
-    formDiv.removeAttribute("class");
-    startBtn.classList.add("hidden");
-  });
-  
-  document.querySelector("form#form-div").addEventListener("submit", handleFormSubmission);
 });
-
-
